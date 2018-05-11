ba32aee3a5df0314f3e4424fa9b6e7ddf5725ae2
nyaachen <killyouabc@live.cn>
2018-05-11 13:26:23 +0800

    fuck g++

--- database.h
+++ database.h
@@ -15,12 +15,22 @@
 
 namespace mySQL {
 
+// 异常处理
 // 单词解析
 bool is_sub_str(const std::string &s1, const std::string &s2) {
   return s2.find(s1) != std::string::npos;
 }
 
 static const std::string IGNORE(" "), SYMBOL("(),=");
+
+class Bad_parse : public std::invalid_argument {
+  public:
+    Bad_parse(const std::string &p) : std::invalid_argument(p) {}
+};
+
+class Word_parser;
+std::string error_identifier(const Word_parser &p);
+std::string error_key(const Word_parser &p);
 
 class Word_parser {
 private:
@@ -32,7 +42,7 @@ public:
     std::string word = parse();
     for (auto &w: word) w = std::toupper(w);
     if (word == keyword) return true;
-    else raise ;
+    else throw Bad_parse(error_key(*this));
   }
   std::string get_keyword() {
     std::string word = parse();
@@ -59,6 +69,22 @@ public:
   std::string get_str() const {return s;}
 };
 
+std::string error_key(const Word_parser &p) {
+  std::string err("读取指令时发生错误\n");
+  err += p.get_str() + "\n";
+  err += std::string(p.get_pos(), ' ');
+  err += "*\n不能识别的关键字";
+  return err;
+}
+std::string error_identifier(const Word_parser &p) {
+  std::string err("读取指令时发生错误\n");
+  err += p.get_str() + "\n";
+  err += std::string(p.get_pos(), ' ');
+  err += "*\n非法的值";
+  return err;
+}
+
+
 // 单词解析 结束
 
 
@@ -67,7 +93,6 @@ public:
 class Record;
 class Table;
 class Named_Table;
-class Parser;
 class Database;
 
 // 数据定义
@@ -84,15 +109,9 @@ public:
   Record(std::vector<Data> l) : record(l) {};
   Record() = default;
 };
-std::ostream &operator<<(std::ostream &os, const Record &r){
-  for (const Data &w: r.record)
-    os << w << "\t" << std::endl;
-  return os;
-}
-std::istream &operator<<(std::istream &is, Record &r){
+std::ostream &operator<<(std::ostream &os, Record &r){
 
 }
-
 
 // 一个表是多条记录的总和
 class Table{
@@ -124,10 +143,6 @@ static const std::vector<std::string> KEYWORDS {"CREATE", "TABLE", "TO", "FROM",
 
 
 
-class Bad_parse : public std::invalid_argument {
-  public:
-    Bad_parse(const std::string &p) : std::invalid_argument(p) {}
-};
 
 class Database {
 private:
@@ -137,44 +152,52 @@ private:
   std::vector<Named_Table> database_list;
   // Parser
   Table parser_result;
-  bool parse_create(Parser &s){
+  bool parse_create(Word_parser &s){
     s.assume("TABLE");
+    std::string name = s.parse();
+    //
+    std::string key= s.get_keyword();
+    if( key == "FROM"){
+      std::string filename = s.parse();
+      s.assume("");
+      // TODO
+    }
+    else if (key == "(" ) {
+      std::vector<std::string> attrib_name;
+      std::set<std::string> dup_check;
+      while (true) {
+        std::string attr = s.parse();
+        if (dup_check.find(attr) == dup_check.end()) {
+          dup_check.insert(attr);
+          attrib_name.push_back(attr);
+        }
+        else throw Bad_parse(error_identifier(s));
+        std::string next = s.get_keyword();
+        if (next == ",") continue;
+        else if (next == ")") break;
+        else throw Bad_parse(error_key(s));
+      }
+    }
+    else throw Bad_parse(error_key(s));
   }
-  bool parse_drop(Parser &s){
+  bool parse_drop(Word_parser &s){
 
   }
-  bool parse_insert(Parser &s){
+  bool parse_insert(Word_parser &s){
 
   }
-  bool parse_delete(Parser &s){
+  bool parse_delete(Word_parser &s){
 
   }
-  bool parse_update(Parser &s){
+  bool parse_update(Word_parser &s){
 
   }
-  bool parse_select(Parser &s){
+  bool parse_select(Word_parser &s){
 
   }
-  // 异常处理
-  std::string error_key(const Word_parser &p) {
-    std::string err("读取指令时发生错误\n");
-    err += p.get_str() + "\n";
-    err += std::string(p.get_pos(), ' ');
-    err += "*\n不能识别的关键字";
-    return err;
-  }
-  std::string error_identifier(const Word_parser &p) {
-    std::string err("读取指令时发生错误\n");
-    err += p.get_str() + "\n";
-    err += std::string(p.get_pos(), ' ');
-    err += "*\n非法的值";
-    return err;
-  }
+
 public:
-  Database(std::string filename) : database_name(filename){
-    // TODO
-  }
-
+  Database();
   // io同步
   bool sync();
 
@@ -183,7 +206,7 @@ public:
     auto p = Word_parser(s);
     std::string start = p.parse();
     if (start == "CREATE") {
-      parse_create(p);
+      //parse_create(s);
       return 0;
     }
     else {
